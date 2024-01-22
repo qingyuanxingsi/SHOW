9c9a44b9fc5a34af6518e94158d0a88101217d8a
doodleliang <doodleliang@tencent.com>
2024-01-22 21:37:22 +0800

    feat(ci): update config

--- configs/base/face_ider.py
+++ configs/base/face_ider.py
@@ -1,10 +1,10 @@
 import os 
+from global_config import FACE_MAIN_DIR
 
-show_dir = os.environ['SHOW_DIR']
 ider_cfg=dict(
     type='arcface_ider',
     threshold=0.40,
     npy_folder_name='arcface',
-    weight=os.path.join(show_dir, 'models/arcface/glink360k_cosface_r100_fp16_0.1.pth'),
+    weight=os.path.join(FACE_MAIN_DIR, 'models/arcface/glink360k_cosface_r100_fp16_0.1.pth'),
     name='r100',fp16=True,det='mtcnn',
 )
--- global_config.py
+++ global_config.py
@@ -2,3 +2,4 @@
 
 SHOW_MAIN_DIR = '/mnt/chongqinggeminiceph1fs/geminicephfs/security-others-common/doodleliang/SHOW'
 EXTRA_MAIN_DIR = SHOW_MAIN_DIR
+FACE_MAIN_DIR = SHOW_MAIN_DIR
